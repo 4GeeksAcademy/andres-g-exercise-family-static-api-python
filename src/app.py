6e4e4f1437c1cdae7b743b66b74b2d49fce8b51e
Andrés Guaipo <andresguaipo.16@gmail.com>
2025-06-09 20:09:31 +0000

    first commit

--- src/app.py
+++ src/app.py
@@ -33,11 +33,50 @@ def sitemap():
 def handle_hello():
     # This is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {"hello": "world",
-                     "family": members}
+    response_body = {"family": members}
     return jsonify(response_body), 200
 
+@app.route('/members/<int:member_id>', methods=['GET'])
+def get_a_member(member_id):
+    try:
+        member = jackson_family.get_member(member_id)
+        if member is None:
+            return jsonify({"msg": "member not found"}), 404
+        return jsonify(member), 200
+    except Exception as e:
+        return jsonify({"error": "Internal Server Error"}), 500
 
+@app.route('/members', methods=['POST'])
+def add_a_member():
+    try:
+        data = request.get_json()
+
+        if not data:
+            return jsonify({"error": "Request body is required"}), 400
+        
+        required_fields = ["first_name", "age", "lucky_numbers"]
+        for field in required_fields:
+            if field not in data:
+                return jsonify({"error": f"Missing required fields: {field}"}), 400
+        
+        if data["age"] <= 0:
+            return jsonify({"error": "The member's age must be greater than 0"}), 400
+        
+        new_member = jackson_family.add_member(data)
+        return jsonify(new_member), 200
+    except Exception as e:
+        return jsonify({"error": "Internal Server Error"}), 500
+    
+@app.route('/members/<int:member_id>', methods=['DELETE'])
+def delete_a_member(member_id):
+    try: 
+        deleted_member = jackson_family.delete_member(member_id)
+        if deleted_member is None: 
+            return jsonify({"error": "Member not found"}), 404
+
+        return jsonify({"done": True}), 200
+    except Exception as e: 
+        return jsonify({"error": "Internal Server Error"}), 500
 
 # This only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
